dab111e58dbc0af8e300b36e7354627c498f7736
aziz <azizmuminov252@gmail.com>
2021-07-10 15:54:42 +0500

    update 1.0

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
     let x = 1000;
     let y = 1000;
+    const img = document.querySelector(`.img`),
+    btn = document.querySelector('.btn');
+
+    
 
     function test(){ 
-    const bloc = document.querySelector('.container'),
-    btn = document.querySelector('.btn'), 
+    const bloc = document.querySelector('.container'), 
     link = document.createElement('h1'),
     br = document.createElement('br');
     scrolldown(bloc.lastChild)    
-    btn.style.display = "none" 
     y = y - 7
     link.innerHTML =  x +  " - 7 =" + y
     link.classList = 'text'
@@ -25,6 +27,8 @@
    }, 100);
 }
 
+
+
 function scrolldown(e) {
     window.scroll({
         left: 0,
@@ -32,6 +36,18 @@ function scrolldown(e) {
     })
 }
 
+function callTest(){
+    btn.style.display = "none" 
+    setTimeout(() => {
+        img.style.display = 'none'
+        test()
+    }, 5000);
+}
 
-
- 
+function playMusic(){
+    img.classList.add('imgOpen')
+    var music = new Audio('files/letmidie.mp3');
+    music.play();
+    }
+  
+    
